a633490fc43f7e6a5e9f0fdb08101dbdb45af975
Oskari Noppa <oskari@arpakuut.io>
2020-11-10 18:24:45 +0200

    Basic runtime API

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -17,9 +17,19 @@ function isObject(val: unknown) {
 	const t = typeof val
 	return t === 'object' || t === 'function'
 }
-
-const slice = [].slice
 const bind = isObject.bind
+const slice = [].slice
+const assign = Object.assign
+const shallowClone = <T extends Record<string, any>>(obj: T): T =>
+	assign({}, obj)
+
+type ProxyPrivateApiContainer = {
+	__proxyfill: {
+		target: object
+		handler: ProxyHandler<object>
+		revokable: boolean
+	}
+}
 
 function createProxy(
 	// eslint-disable-next-line @typescript-eslint/ban-types
@@ -34,8 +44,15 @@ function createProxy(
 		)
 	}
 
+	const proxyPrivateApi: ProxyPrivateApiContainer = {
+		__proxyfill: {
+			target,
+			handler: shallowClone(handler),
+			revokable,
+		},
+	}
+
 	let proxy: any
-	let type: 'function' | 'array'
 	if (typeof target === 'function') {
 		proxy = function ProxyPolyfill(this: any) {
 			const usingNew = this && this.constructor === proxy
@@ -56,14 +73,102 @@ function createProxy(
 			}
 			return target.apply(this, args)
 		}
-		type = 'function'
+		assign.call(proxy, proxyPrivateApi)
 	} else if (target instanceof Array) {
 		proxy = []
-		type = 'array'
+		assign.call(proxy, proxyPrivateApi)
+	} else {
+		proxy = proxyPrivateApi
 	}
 
 	return proxy
 }
 
-export function get(obj, property) {}
-export function set(obj, property, value) {}
+type PossiblyProxy = null | undefined | Partial<ProxyPrivateApiContainer>
+
+function getProxyfillApi(
+	obj: PossiblyProxy
+): null | ProxyPrivateApiContainer['__proxyfill'] {
+	const type = typeof obj
+	return (
+		(((type === 'object' && obj !== null) || type === 'function') &&
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			obj!.__proxyfill) ||
+		null
+	)
+}
+
+export function isNotProxy(obj: PossiblyProxy) {
+	return !getProxyfillApi(obj)
+}
+
+const privateApiKey: keyof ProxyPrivateApiContainer = '__proxyfill'
+
+/**
+ *
+ * @param obj Target object to get a value from, possibly a "Proxy" created with createProxy.
+ * @param property Name of the property to get
+ * @param notProxy Result of calling isNotProxy(obj) that can be used as an optimization
+ * 		to skip the proxy check in places where properties of a constant object are accessed
+ * 		multiple times.
+ */
+export function get(
+	obj: PossiblyProxy,
+	property: string,
+	notProxy: boolean
+): any {
+	if (property === privateApiKey) {
+		throw new Error('Cannot access private API of proxyfill library')
+	}
+	const api = !notProxy && getProxyfillApi(obj)
+	if (api) {
+		const handlers = api.handler
+		const getHandler = handlers.get
+		if (getHandler) {
+			return getHandler.call(handlers, api.target, property, obj)
+		}
+	}
+
+	return (obj as any)[property]
+}
+
+export function set(
+	obj: PossiblyProxy,
+	property: string,
+	value: any,
+	notProxy: boolean
+): any {
+	if (property === privateApiKey) {
+		throw new Error('Cannot access private API of proxyfill library')
+	}
+	const api = !notProxy && getProxyfillApi(obj)
+	if (api) {
+		const handlers = api.handler
+		const setHandler = handlers.set
+		if (setHandler) {
+			return setHandler.call(handlers, api.target, property, value, obj)
+		}
+	}
+
+	return ((obj as any)[property] = value)
+}
+
+export function ProxyPolyfill(
+	this: any,
+	target: object,
+	handler: ProxyHandler<object>
+) {
+	const isCalledWithNew = !!this && this instanceof ProxyPolyfill
+	if (!isCalledWithNew) {
+		throw new TypeError("Constructor Proxy requires 'new'")
+	}
+
+	return createProxy(target, handler, false)
+}
+
+ProxyPolyfill.revocable = function (
+	target: object,
+	handler: ProxyHandler<object>
+) {
+	return createProxy(target, handler, true)
+}
