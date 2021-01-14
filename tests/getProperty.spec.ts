fe840c7881522e100a86f526616e66d8d1a40c35
Oskari Noppa <oskari@arpakuut.io>
2021-01-14 21:58:35 +0200

    Add more specs

--- tests/getProperty.spec.ts
+++ tests/getProperty.spec.ts
@@ -4,4 +4,19 @@ describe('proxyfill Babel plugin', () => {
 	it('should transform getters', () => {
 		expect(traverse('foo.bar')).toMatchSnapshot()
 	})
+	it('should transform computed getters', () => {
+		expect(
+			traverse(`
+			foo[0]
+			foo[bar]
+			foo[bar.baz]
+		`)
+		).toMatchSnapshot()
+	})
+	it('should transform method calls', () => {
+		expect(traverse('foo.bar()')).toMatchSnapshot()
+	})
+	it('should not transform direct function calls', () => {
+		expect(traverse('bar()')).toMatchSnapshot()
+	})
 })
