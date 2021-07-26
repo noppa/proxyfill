4f25b88c8290a642e7638b58e2a4b482806ffd52
Oskari Noppa <oskari@arpakuut.io>
2021-07-26 14:30:12 +0300

    Add has and deleteProperty exports to global bundle

--- src/runtime/global.ts
+++ src/runtime/global.ts
@@ -7,3 +7,5 @@ context.Proxy = proxyfill.Proxy
 context._proxyfillRuntime$get = proxyfill.get
 context._proxyfillRuntime$set = proxyfill.set
 context._proxyfillRuntime$invoke = proxyfill.invoke
+context._proxyfillRuntime$has = proxyfill.has
+context._proxyfillRuntime$deleteProperty = proxyfill.deleteProperty
